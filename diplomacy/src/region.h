4c3017e22f58ee595a6b1b100c1b9deace8877dd
Tim Quelch <tim@quelch.name>
2018-12-07 13:28:31 +1000

    Add abbreviation to region class

--- diplomacy/src/region.h
+++ diplomacy/src/region.h
@@ -10,18 +10,22 @@ namespace diplomacy {
         enum class Type { land, coast, sea };
 
         Region() noexcept = default;
-        Region(std::string_view name, bool isSc, Type type)
-            : name_{name}, isSc_{isSc}, type_{type} {}
+        Region(std::string_view name, std::string_view abbr, bool isSc, Type type) noexcept
+            : name_{name}, abbr_{abbr}, isSc_{isSc}, type_{type} {}
 
-        [[nodiscard]] auto name() const { return name_; }
+        [[nodiscard]] auto name() const noexcept { return name_; }
+        [[nodiscard]] auto abbr() const noexcept { return abbr_; }
         [[nodiscard]] auto isSc() const noexcept { return isSc_; }
         [[nodiscard]] auto type() const noexcept { return type_; }
 
     private:
         std::string name_ = {};
+        std::string abbr_ = {};
         bool isSc_ = false;
         Type type_ = Type::land;
     };
+
+    [[nodiscard]] bool operator==(Region const& lhs, Region const& rhs) noexcept;
 
     static_assert(std::is_nothrow_default_constructible_v<Region>);
     static_assert(std::is_copy_constructible_v<Region>);
