a3e5a7c4f154ca6d05493cbe16cc191123fb2864
Tim Quelch <tim@quelch.name>
2018-12-10 21:49:42 +1000

    Add initial unit class

---
+++ diplomacy/src/unit.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "region.h"
+
+#include <stdexcept>
+
+#include <fmt/format.h>
+
+namespace diplomacy {
+    class Unit {
+    public:
+        enum class Type { army, fleet };
+
+        constexpr Unit() noexcept = default;
+        constexpr Unit(Type type, Region* region) noexcept : type_{type}, region_{region} {}
+
+        [[nodiscard]] constexpr auto type() const noexcept { return type_; }
+        [[nodiscard]] auto region() const {
+            if (!region_) {
+                throw std::runtime_error{"Dereferencing Region pointer which is null"};
+            }
+            return *region_;
+        }
+
+    private:
+        Type type_ = Type::army;
+        Region* region_ = nullptr;
+    };
+
+    static_assert(std::is_copy_constructible_v<Unit>);
+
+    static_assert(std::is_nothrow_default_constructible_v<Unit>);
+    static_assert(std::is_nothrow_move_constructible_v<Unit>);
+    static_assert(std::is_nothrow_destructible_v<Unit>);
+    static_assert(std::is_nothrow_swappable_v<Unit>);
+
+    static_assert(std::is_trivially_copy_constructible_v<Unit>);
+    static_assert(std::is_trivially_move_constructible_v<Unit>);
+    static_assert(std::is_trivially_destructible_v<Unit>);
+} // namespace diplomacy
