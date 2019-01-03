5fd85bac90a56d852cb40c35fa736d8360d74d17
Tim Quelch <tim@quelch.name>
2019-01-03 20:14:40 +1000

    Add basic player class

---
+++ diplomacy/src/player.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "unit.h"
+
+#include <string>
+#include <vector>
+
+namespace diplomacy {
+    class Player {
+    public:
+        Player() = delete;
+        Player(std::string_view name, std::string_view empireName) noexcept
+            : name_{name}, empireName_{empireName} {}
+
+        [[nodiscard]] auto name() const noexcept { return name_; }
+        [[nodiscard]] auto empireName() const noexcept { return empireName_; }
+
+    private:
+        std::string name_ = {};
+        std::string empireName_ = {};
+        std::vector<Unit> units_ = {};
+    };
+
+    static_assert(!std::is_default_constructible_v<Player>);
+    static_assert(std::is_copy_constructible_v<Player>);
+
+    static_assert(std::is_nothrow_move_constructible_v<Player>);
+    static_assert(std::is_nothrow_destructible_v<Player>);
+    static_assert(std::is_nothrow_swappable_v<Player>);
+} // namespace diplomacy
